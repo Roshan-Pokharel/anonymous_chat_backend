7589c434efcf0d93a93303649a2027f256e954a1
roshan <roshanpokharel890@gmail.com>
2025-08-03 18:58:50 +0545

    small solution

--- root/server.js
+++ root/server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 // --- CORS Configuration ---
 const corsOptions = {
-  origin: "https://anonymous-chat-frontend-gray.vercel.app/",
+  origin: "*",
   methods: ["GET", "POST"],
 };
 
